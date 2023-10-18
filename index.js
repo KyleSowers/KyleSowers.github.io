0c7bf3ed5d0bba31796c87beac50a15b2598c352
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-18 09:45:53 -0500

    got rid of few uneeded lines

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ let myPhoto = document.querySelector('.my-photo');
 
 chip1.addEventListener('mouseenter', () => {
     myPhoto.src = './images/honor-guard.jpg'; // Change the image source on hover over .chip1
-    // myPhoto.style.transform = 'scale(1)'; // Scale up the image on hover over .chip1
+    myPhoto.style.transform = 'scale(1)'; // Scale up the image on hover over .chip1
     myPhoto.style.transition = 'transform 2s ease'; // Add a transition for smooth scaling
 });
 
 chip1.addEventListener('mouseleave', () => {
     myPhoto.src = './images/codeup-photo1 copy.jpg'; // Restore the original image source when the mouse leaves .chip1
-    // myPhoto.style.transform = 'scale(1)'; // Reset the scale to its original size
+    myPhoto.style.transform = 'scale(1)'; // Reset the scale to its original size
 });
 
 const chip2 = document.querySelector('.chip2');
@@ -19,7 +19,7 @@ myPhoto = document.querySelector('.my-photo');
 
 chip2.addEventListener('mouseenter', () => {
     myPhoto.src = './images/amu-mascot.jpeg'; // Change the image source on hover over .chip1
-    // myPhoto.style.transform = 'scale()'; // Scale up the image on hover over .chip1
+    myPhoto.style.transform = 'scale()'; // Scale up the image on hover over .chip1
     myPhoto.style.transition = 'transform 2s ease'; // Add a transition for smooth scaling
 });
 
